ec847d7878d2f778ee72b71623cd53227d8bd7d5
wardou2 <ward92@gmail.com>
2019-07-03 11:23:22 -0700

    maybe actually fixed githubs migrations this time

--- db/migrate/20190524220403_create_githubs.rb
+++ db/migrate/20190524220403_create_githubs.rb
@@ -1,8 +1,8 @@
 class CreateGithubs < ActiveRecord::Migration[5.2]
   def change
     create_table :githubs do |t|
-      t.string :repo_url
-      t.string :name
+      t.string :repo_owner
+      t.string :repo_name
       t.string :img_url
       t.string :summary
       t.integer :user_id
--- db/migrate/20190703181217_add_repo_url_front_to_githubs.rb
+++ db/migrate/20190703181217_add_repo_url_front_to_githubs.rb
@@ -1,7 +1,9 @@
 class AddRepoUrlFrontToGithubs < ActiveRecord::Migration[5.2]
   def change
-    remove_column :githubs, :repo_url
-    
+    remove_column :githubs, :repo_owner
+    remove_column :githubs, :repo_name
+
+    add_column :githubs, :name, :string
     add_column :githubs, :repo_url_front, :string
     add_column :githubs, :repo_url_back, :string
     add_column :githubs, :demo_url, :string
