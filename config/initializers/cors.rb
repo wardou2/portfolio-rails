91dbe60bea3192584c0d9a31f22d706201723859
wardou2 <ward92@gmail.com>
2019-10-22 12:07:59 -0700

    add custom domain whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins 'https://douglasward-portfolio.herokuapp.com', 'localhost:3001', 'https://douglasoward.com'
+    origins 'https://douglasward-portfolio.herokuapp.com', 'localhostrails:3001', 'https://douglasoward.com'
 
     resource '*',
       headers: :any,
