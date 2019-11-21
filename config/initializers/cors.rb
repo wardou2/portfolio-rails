7930b82559a4f2bbd88b79bd584ac353cddd1e0e
wardou2 <ward92@gmail.com>
2019-11-21 16:14:52 -0500

    bump gems

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
-    origins 'https://douglasward-portfolio.herokuapp.com', 'https://www.douglasoward.com', 'http://www.douglasoward.com'
+    origins 'https://douglasward-portfolio.herokuapp.com', 'https://www.douglasoward.com', 'http://www.douglasoward.com', 'localhost:3001'
 
     resource '*',
       headers: :any,
